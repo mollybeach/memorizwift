9ac6c955e3a24b259367e06ac5db04ec46ac5b15
mollybeach <mollyjbeach@gmail.com>
2024-10-17 12:02:38 -0700

    refactor: Adjust layout and update file references in ContentView and project settings
    - Updated the minimum size for the adaptive grid in `LazyVGrid` from 85 to 65 for better UI scaling.
    - Added a blue background to the "Shuffle" button and a yellow background to the parent view for improved visual distinction.
    - Updated project file references and build configurations, including `ContentView.swift` and `MemorizwiftTests.swift`, to ensure proper file inclusion in the build phases.

--- Memorizwift/ContentView.swift
+++ Memorizwift/ContentView.swift
@@ -19,12 +19,14 @@ struct ContentView: View {
             Button("Shuffle") {
                 viewModel.shuffle()
             }
+            .background(Color.blue)
         }
+        .background(.yellow)
         .padding()
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 65), spacing: 0)], spacing: 0) {
             
             ForEach(viewModel.cards) { card in
                 CardView(card)
