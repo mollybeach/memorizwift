6548716b4a2bec1ec8eb1c5f40fc8f99bee13b30
mollybeach <mollyjbeach@gmail.com>
2024-10-22 13:46:25 -0700

    fix: Set default card face-up state to false
    - Changed the default value of `isFaceUp` from `true` to `false` in `MemoryGame.Card` to ensure cards start face down.

--- Memorizwift/MemoryGame.swift
+++ Memorizwift/MemoryGame.swift
@@ -45,7 +45,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
-        var isFaceUp = true
+        var isFaceUp = false
         var isMatched = false
         let content: CardContent
         var id: String
